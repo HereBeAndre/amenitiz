0e6c47b5e3799b2c30ac2774506a7485a8f601bd
pizzaguyyy <andrea.tota@quinyx.com>
2025-07-01 21:04:51 +0200

    Fix layout of Home content section

--- src/pages/Home/partials/HomeContent/index.tsx
+++ src/pages/Home/partials/HomeContent/index.tsx
@@ -1,16 +1,16 @@
 import { PlayerList } from "../../../../shared/PlayerList";
 import type { TPlayers } from "../../../../types";
 
+import styles from "./index.module.scss";
+
 interface HomeContentProps {
   players: TPlayers;
   loading: boolean;
   error: Error | null;
 }
 
-export const HomeContent = ({ players, loading, error }: HomeContentProps) => {
-  if (loading) return <p>Loading...</p>;
-
-  if (error) return <p>Error: {error.message}</p>;
-
-  return <PlayerList players={players}></PlayerList>;
-};
+export const HomeContent = ({ players, loading, error }: HomeContentProps) => (
+  <div className={styles.homeContent}>
+    <PlayerList players={players} isLoading={loading} error={error} />
+  </div>
+);
--- src/shared/PlayerList/index.tsx
+++ src/shared/PlayerList/index.tsx
@@ -6,14 +6,22 @@ import styles from "./index.module.scss";
 
 interface PlayerListProps {
   players: TPlayers;
+  isLoading: boolean;
+  error: Error | null;
 }
 
-export const PlayerList = ({ players }: PlayerListProps) => (
-  <ul className={styles.playerList}>
-    {players?.map((username) => (
-      <li key={username} className={styles.playerListItem}>
-        <LinkComponent to={`/player/${username}`}>{username}</LinkComponent>
-      </li>
-    ))}
-  </ul>
-);
+export const PlayerList = ({ players, isLoading, error }: PlayerListProps) => {
+  if (isLoading) return <p>Loading...</p>;
+
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <ul className={styles.playerList}>
+      {players?.map((username) => (
+        <li key={username} className={styles.playerListItem}>
+          <LinkComponent to={`/player/${username}`}>{username}</LinkComponent>
+        </li>
+      ))}
+    </ul>
+  );
+};
